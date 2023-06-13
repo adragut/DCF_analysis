0544b308cb822e30719aac66190c719540f77870
Alexandru Dragut <alexandru.marius.dragut@gmail.com>
2023-06-13 12:48:34 +0300

    Seaking Alpha data extract

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DCF_analysis
+
+#Retrieving financial data from Rapid API via Python.
+extract_data.py 
+
+
+
+
 Django 
---
+++ extract_data.py
@@ -0,0 +1,38 @@
+import requests
+
+# Set up API endpoint URL
+base_url = 'https://seeking-alpha.p.rapidapi.com/'
+endpoint_url = base_url + 'symbols/get-financials'
+
+headers = {'X-RapidAPI-Key':'6a1269eb60msh80257fe6797736cp147a07jsnb276f98fded5',
+    'X-RapidAPI-Host':'seeking-alpha.p.rapidapi.com'
+}
+
+# Retrieve target stock ticker and desired currency
+stock_ticker = input("Enter the target stock ticker: ")
+period_type = input("One of the following : annual|quarterly|ttm")
+statement_type = input("One of the following : income-statement|balance-sheet|cash-flow-statement")
+target_currency = input("Enter target currency: USD or e.g. EUR\n")
+if target_currency not in ["USD", "EUR"]:
+    print('Error: Invalid currency')
+else:
+    target_currency = target_currency.upper()
+
+# Format Rapid API request payload with necessary parameters
+payload = {'symbol': f'{stock_ticker}','target_currency':f'{target_currency}','period_type':f'{period_type}','statement_type':f'{statement_type}'}
+if target_currency != 'USD':
+    payload['foreign_exchange'] = True
+
+# Send GET request to retrieve data
+response = requests.get(endpoint_url, params=payload, headers=headers)
+
+if response.status_code == 200:
+    # Parse json response
+    json_data = response.json()
+else:
+    # Handle errors
+    print('Unexpected status code: {}'.format(response.status_code))
+
+# Print response JSON object
+print(response.json())
+
